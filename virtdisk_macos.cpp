573742aa059e7000a02f207d3accf366cb745511
Igor Goremykin <leg3nd1991@gmail.com>
2025-03-28 20:14:30 +0200

    add comments to FUSE example

--- virtdisk_macos.cpp
+++ virtdisk_macos.cpp
@@ -1,5 +1,11 @@
 #ifdef __APPLE__
 
+// Example source:
+// https://github.com/macfuse/demo/blob/master/LoopbackFS-C/loopback/loopback.c
+
+// Docan FUSE operations mapping
+// https://github.com/dokan-dev/dokany/wiki/FUSE
+
 #include "virtdisk.h"
 
 VirtDisk::VirtDisk(const Connection& conn) : conn(conn)

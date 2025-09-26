461ce444066bfdb0f220dd512f772bff90c9d711
Igor Goremykin <leg3nd1991@gmail.com>
2025-09-26 22:09:40 +0300

    add rest of GetattrResult fields to FUSEBackend_spec (broken)

--- test/fusebackend_spec.h
+++ test/fusebackend_spec.h
@@ -19,8 +19,23 @@ private slots:
 
         QFile appIcon = QFile(appIconPath);
         QCOMPARE(appIcon.exists(), true);
+
         QCOMPARE(result->status, 0);
 //        QCOMPARE(result->st_dev, 0);
+        QCOMPARE(result->st_ino, 0);
+        QCOMPARE(result->st_nlink, 0);
+        QCOMPARE(result->st_mode, 0);
+        QCOMPARE(result->st_uid, 0);
+        QCOMPARE(result->st_gid, 0);
+        QCOMPARE(result->st_rdev, 0);
         QCOMPARE(result->st_size, 130121);
+        QCOMPARE(result->st_blksize, 0);
+        QCOMPARE(result->st_blocks, 0);
+        QCOMPARE(result->st_atim.tv_sec, 0);
+        QCOMPARE(result->st_atim.tv_nsec, 0);
+        QCOMPARE(result->st_mtim.tv_sec, 0);
+        QCOMPARE(result->st_mtim.tv_nsec, 0);
+        QCOMPARE(result->st_ctim.tv_sec, 0);
+        QCOMPARE(result->st_ctim.tv_nsec, 0);
     }
 };

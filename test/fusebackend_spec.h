aa5724cf02d5ce1a044d5193d3ea57249d51a1d3
Igor Goremykin <leg3nd1991@gmail.com>
2025-09-29 22:06:16 +0300

    remove useless Writes_data_to_file assert at FUSEBackend_spec

--- test/fusebackend_spec.h
+++ test/fusebackend_spec.h
@@ -179,10 +179,7 @@ private slots:
         file.read(fileData, strlen(data));
         file.close();
         fileData[strlen(data)] = '\0';
-        bool fileExists = file.exists();
-        QFile::remove(filePath);
 
-        QVERIFY(fileExists);
         QCOMPARE(result, strlen(data));
         QCOMPARE(strcmp(fileData, data), 0);
     }
